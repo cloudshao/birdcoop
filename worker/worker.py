ec55ce16088d54575207af3db914bba90f9d7304
Cloud Shao <me@cloudshao.com>
2010-12-05 14:36:36 -0800

    Worker portion of #3: Implemented returning response without followers/followees when user to crawl is private On socketerrors, we now allow execution to stop since we probably don't want to keep trying 150 times. It'll try again next hour anyway

--- worker/worker.py
+++ worker/worker.py
@@ -8,29 +8,30 @@ ANNOUNCE_PORT = 5630
 REPLY_PORT = 5631
 
 for i in range(2):
+   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   sock.connect((HOST, ANNOUNCE_PORT))
+   user = sock.recv(1024)
+   
    try:
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      sock.connect((HOST, ANNOUNCE_PORT))
-      user = sock.recv(1024)
 
+      # Get the followers and followees from twitter
       followers = json.loads(request.getFollowersJson(int(user)))
       followees = json.loads(request.getFolloweesJson(int(user)))
       response = json.dumps({'user':user,
                              'followers':followers,
                              'followees':followees,})
 
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      sock.connect((HOST, REPLY_PORT))
-      sock.send(response)
-      sock.close()
-
-      print 'total: ' + str(i) + ', just crawled: ' + user
-
    except urllib2.HTTPError, e:
+
+      # If the user is private, respond without followers/followees
       if e.code == 401:
-         pass
+         response = json.dumps({'user':user})
       else:
          raise
 
-   except socket.error, e:
-      print e
+   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   sock.connect((HOST, REPLY_PORT))
+   sock.send(response)
+   sock.close()
+
+   print 'total: ' + str(i) + ', just crawled: ' + user
