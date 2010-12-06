606f14f4be52fd3d78cdadcc9d52ee71aa9bafee
Cloud Shao <me@cloudshao.com>
2010-12-05 17:34:42 -0800

    Added worker bootstrap script to version control just so that it's easy to access. It goes in cron.hourly, so it can't be updated automatically on all nodes

---
+++ worker/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is for worker Planetlab nodes to update themselves and
+# start their worker.sh script (which is under version control)
+# This typically goes in /etc/cron.hourly/
+
+# Update the code by pulling the repo
+cd /home/usf_ubc_gnutella1/birdcoop
+git reset --hard HEAD
+git pull
+
+# Since this node is a worker, execute the worker script
+cd /home/usf_ubc_gnutella1/birdcoop/worker
+./worker.sh
