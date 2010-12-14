6c2ae1173fc84f849bc4ac2980d1faaf74997447
Cloud Shao <me@cloudshao.com>
2010-12-13 17:59:18 -0800

    Encapsulated the database functionality into its own class

--- master/server.py
+++ master/server.py
@@ -5,110 +5,21 @@ import SocketServer
 import string
 import threading
 import time
+import sql
 import sqlite3
 import sys
 import simplejson as json
 import gc
 
-def init(cursor):
-
-	#check to see if user table was created
-	cursor.execute("select name from sqlite_master Where type='table' and Name= 'user_table'")
-
-	if cursor.fetchone() == None :
-		# user table has not yet been created, create it
-		print"Creating user table"
-		cursor.execute("create table user_table (user_id integer PRIMARY KEY, name char(20), location char(30), bio char(160), currTime INTEGER)")
-
-
-	#check to see if user crawled table was created
-	cursor.execute("select name from sqlite_master Where type='table' and Name= 'user_crawled_table'")
-
-	if cursor.fetchone() == None :
-		# user crawled table has not yet been created, create it
-		print"Creating user crawled table"
-		# this table tracks if a user has been crawled or not
-		cursor.execute("create table user_crawled_table (user_id integer PRIMARY KEY, crawled integer, currTime  integer)")
-
-	# check to see if followers table has been created
-	cursor.execute("select name from sqlite_master Where type='table' and Name= 'follower_table'")
-
-	if cursor.fetchone() == None :
-		# followers table has not yet been created, create it
-		print"Creating follower table"
-		cursor.execute("create table follower_table (user_id integer, follower_id integer, currTime INTEGER, PRIMARY KEY(user_id, follower_id))")
-
-	# check to see if tweet table has been created
-	cursor.execute("select name from sqlite_master Where TYPE='table' and NAME = 'tweet_table'")
-
-	if cursor.fetchone() == None :
-		print"Creating tweet table"
-		# tweet table has not yet been created, create it
-		cursor.execute("create table tweet_table (user_id integer, time CHAR(20), tweet CHAR(140), currTime INTEGER, PRIMARY KEY(user_id, time))")
-
-# finds a users which are not crawlwed and returns them
-def select_unfollowed_users(cursor):
-	cursor.execute('Select user_id from user_crawled_table where crawled == 0')
-	user = cursor.fetchall()
-	return user;
-
-# inserts one user into user table
-def insert_user(cursor, user_id, name, location, bio) :
-	try:
-		#print"Try inserting " + str(user_id)
-		t = (user_id, name, location, bio, int(time.time()))
-		cursor.execute('Insert into user_table(user_id, name, location, bio, currTime) values(?,?,?,?,?)', t)
-	except sqlite3.IntegrityError:
-		# the user was already in the table - so just continue
-		pass
-
-# inserts one user into user_crawled_table - used for for finding users which haven't been craweld
-def insert_user_crawled(cursor, user_id, wasCrawled) :
-	try:
-		#print"Try inserting if " + str(user_id) + " was crawled"
-		t = (user_id, wasCrawled, int(time.time()))
-		cursor.execute('Insert into user_crawled_table(user_id, crawled, currTime) values(?,?,?)', t)
-	except sqlite3.IntegrityError :
-		if wasCrawled  == 1:
-			i  =1
-			# User is already in DB, update wasCrawled if it is now true - (ie this is the user we are crawling) - not in all cases because we don't want to set a 
-			# crawled user back to false
-			#print"User already in db, but it's now crawled so setting it to crawled"
-			cursor.execute('Insert or Replace into user_crawled_table(user_id, crawled, currTime) values(?,?,?)', t)
-
-
-# inserts a user- follower relation into the follower_table
-def insert_follower(cursor, user_id, follower_id) :
-	try:
-		t = (user_id, follower_id, int(time.time()))
-		cursor.execute('Insert into follower_table(user_id, follower_id, currTime) values(?, ?,?)', t)
-	except sqlite3.IntegrityError :
-		pass
-		#print "Tried to insert follower relation we had already inserted"
-
-# inserts a user-tweet relation into the db
-def insert_tweet(cursor, user_id, tweet_time, tweet) :
-	try:
-		t = (user_id, tweet_time, tweet, time.time())
-		cursor.execute('Insert into tweet_table(user_id, time, tweet, currTime) values (?, ?, ?, ?)', t)
-		#print "tweet inserted for" + str(user_id)
-	except sqlite3.IntegrityError:
-		pass
-		#print "tried to insert tweet we had already inserted"
-
-
 u_id = ""
 
 # initialize the connection, and start up our crawl list, also make the lock for renewing the crawl list
-conn = sqlite3.connect("awesomeDB")
-cursor = conn.cursor()
-init(cursor)
-crawl_list  = select_unfollowed_users(cursor)
+database = sql.AwesomeDatabase()
+database.create_tables()
+database.save()
+crawl_list = database.select_unfollowed_users()
+database.close()
 crawl_count = 0
-cursor.close()
-conn.commit()
-
-conn.close()
 
 
 normal_start = 0
@@ -164,14 +75,11 @@ class GetPersonToCrawlHandler(SocketServer.BaseRequestHandler):
 			if len(crawl_list) == 0 or crawl_count > 50:
 				#put data in database
 				#populate our crawl list
-				self.conn = sqlite3.connect("awesomeDB")
-				self.cursor = self.conn.cursor()
-				self.conn.commit()
+				self.database = sql.AwesomeDatabase()
 				self.parse_data()
 				if len(crawl_list) == 0:
-					crawl_list = select_unfollowed_users(self.cursor);
-				self.cursor.close()
-				self.conn.close()
+					crawl_list = self.database.select_unfollowed_users()
+				self.database.close()
 				
 		finally:
 			lock.release()
@@ -191,7 +99,7 @@ class GetPersonToCrawlHandler(SocketServer.BaseRequestHandler):
 		print "About to parse data and populate the database"
 		# Each machine can only crawl 150 followers per hour, so track the number of users crawled
 		# Depending on the rate responses come in, we may need to lock so that we don't loop infinitely
-		while len(responses) > 0 :
+		while len(responses) > 0:
 			user_data = responses.pop()
 			user_id = user_data['user']
 			
@@ -201,13 +109,11 @@ class GetPersonToCrawlHandler(SocketServer.BaseRequestHandler):
 				for user in follower_data :
 					pass
 					# insert the users data into our user_table
-					insert_user(self.cursor, user['id'], user['name'], user['location'], user['description'])
-					# AFAIK this user has not been crawled yet - if so insert_user_crawled will handle it
-					insert_user_crawled(self.cursor, user['id'], 0)
+					self.database.insert_user(user['id'], user['name'], user['location'], user['description'], 0)
 					# this user follows user_name - insert his/her in followers
-					insert_follower(self.cursor, user_id, user['id'])
+					self.database.insert_follower(user_id, user['id'])
 					try: 
-						insert_tweet(self.cursor, user['id'], user['status']['created_at'], user['status']['text']) #Try inserting this users status - if he has one it will insert
+						self.database.insert_tweet(user['id'], user['status']['created_at'], user['status']['text']) #Try inserting this users status - if he has one it will insert
 					except KeyError:
 						# User does not have a status
 						pass
@@ -217,26 +123,22 @@ class GetPersonToCrawlHandler(SocketServer.BaseRequestHandler):
 				followee_data = user_data['followees']
 				for user in followee_data:
 					#insert the users data into our user_table
-					insert_user(self.cursor, user['id'], user['name'], user['location'], user['description'])
-					# AFAIK this user has not been crawled yet - if so insert_user_crawled will handle it
-					insert_user_crawled(self.cursor, user['id'], 0)
+					self.database.insert_user(user['id'], user['name'], user['location'], user['description'], 0)
 					# this user follows user_name - insert him/her in follower_table
-					insert_follower(self.cursor, user['id'], user_id)
+					self.database.insert_follower(user['id'], user_id)
 					try: 
-						i = 1
-						pass
-						insert_tweet(self.cursor, user['id'], user['status']['created_at'], user['status']['text']) #Try inserting this users status - if he has one it will insert
+						self.database.insert_tweet(user['id'], user['status']['created_at'], user['status']['text']) #Try inserting this users status - if he has one it will insert
 					except KeyError:
 						# User does not have a status
 						pass
 
 			# This user with username user_name has now been crawled
-			insert_user_crawled(self.cursor, user_id, 1)
+			self.database.set_crawled(user_id)
 
 		# write everything we have added to disk
 		crawl_count = 0
 		print "about to commit"
-		self.conn.commit()
+		self.database.save()
 		print "Data comitted to DB"
 
 
@@ -434,7 +336,6 @@ if __name__ == '__main__':
 		elif 'heap' in line:
 			print guppy.hpy().heap()
 		elif 'exit' in line:
-			conn.close()
 			recoveryFile = open('recoverycheck', 'w')
 			recoveryFile.write('0') #need to set recovery file to 0, which means we closed properly
 			recoveryFile.close()
---
+++ master/sql.py
@@ -0,0 +1,74 @@
+import sqlite3
+import time
+
+class AwesomeDatabase():
+
+   def __init__(self):
+      self.conn = sqlite3.connect('awesomeDB')
+
+   def save(self):
+      self.conn.commit()
+
+   def close(self):
+      self.conn.close()
+
+   def table_exists(self, table_name):
+      cursor = self.conn.cursor()
+      cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=(?)", (table_name,))
+      exists = cursor.fetchone() != None
+      cursor.close()
+      return exists
+
+   def create_tables(self):
+      cursor = self.conn.cursor()
+
+      if not self.table_exists('user_table'):
+         print"Creating user table"
+         cursor.execute("CREATE TABLE user_table (user_id INTEGER PRIMARY KEY, name CHAR(20), location CHAR(30), bio CHAR(160), crawled INTEGER, currTime INTEGER)")
+
+      if not self.table_exists('follower_table'):
+         print"Creating follower table"
+         cursor.execute("CREATE TABLE follower_table (user_id INTEGER, follower_id INTEGER, currTime INTEGER, PRIMARY KEY(user_id, follower_id))")
+
+      if not self.table_exists('tweet_table'):
+         print"Creating tweet table"
+         cursor.execute("CREATE TABLE tweet_table (user_id INTEGER, time CHAR(20), tweet CHAR(140), currTime INTEGER, PRIMARY KEY(user_id, time))")
+
+      cursor.close()
+
+   def select_unfollowed_users(self):
+      ''' Gets a list of users that have not been crawled '''
+      cursor = self.conn.cursor()
+      cursor.execute('SELECT user_id FROM user_table WHERE crawled=0')
+      users = cursor.fetchall()
+      cursor.close()
+      return users
+
+   def insert_user(self, user_id, name, location, bio, crawled):
+      ''' Inserts a user '''
+      cursor = self.conn.cursor()
+      try: cursor.execute('INSERT INTO user_table(user_id, name, location, bio, crawled, currTime) VALUES(?,?,?,?,?,?)',
+                          (user_id, name, location, bio, crawled, int(time.time()), ))
+      except sqlite3.IntegrityError: pass
+      finally: cursor.close()
+
+   def set_crawled(self, user_id):
+      ''' Sets a user to 'crawled' '''
+      cursor = self.conn.cursor()
+      try: cursor.execute('UPDATE user_table SET crawled=1 WHERE user_id=(?)', (user_id,))
+      finally: cursor.close()
+
+   def insert_follower(self, user_id, follower_id):
+      ''' Inserts a follower-followee relation '''
+      cursor = self.conn.cursor()
+      try: cursor.execute('INSERT INTO follower_table(user_id, follower_id, currTime) VALUES(?,?,?)',
+                          (user_id, follower_id, int(time.time()), ))
+      except sqlite3.IntegrityError: pass
+      finally: cursor.close()
+
+   def insert_tweet(self, user_id, tweet_time, tweet):
+      cursor = self.conn.cursor()
+      try: cursor.execute('INSERT INTO tweet_table(user_id, time, tweet, currTime) VALUES(?,?,?,?)',
+                          (user_id, tweet_time, tweet, int(time.time()), ))
+      except sqlite3.IntegrityError: pass
+      finally: cursor.close()
